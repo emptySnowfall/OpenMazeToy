7bedce007adcd67e2a764a159acd271decba45ee
emptySnowfall <sskrimblo@gmail.com>
2026-01-11 11:33:37 -0600

    {MAZE-002] - minor refactor
    change things up to use proper JS classes, add some comments and cleanup. Might have to transfer most of this logic to React, but that shouldn't be too bad

--- scripts/maze.js
+++ scripts/maze.js
@@ -4,6 +4,12 @@
 	OPEN SOURCE
 */
 
+// constraints for the reasonable size range of a maze map
+const MIN_WIDTH = 6;
+const MAX_WIDTH = 24;
+const MIN_HEIGHT = 6;
+const MAX_HEIGHT = 24;
+
 /*
 	'Cell' Objects store all information needed to:
 	- generate a map
@@ -18,8 +24,53 @@ class Cell {
 		this._eastWall = true;
 		this._southWall = true;
 		this._westWall = true;
-		this._visited = false;
 		this._activeSpot = false;
+		this._visited = false;
+	}
+	// getters and setters for North wall state
+	getNorthWallState() {
+		return this._northWall;
+	}
+	setNorthWallState(newState) {
+		this._northWall = newState;
+	}
+	// getters and setters for East wall state
+	getEastWallState() {
+		return this._eastWall;
+	}
+	setEastWallState(newState) {
+		this._eastWall = newState;
+	}
+	// getters and setters for South wall state
+	getSouthWallState() {
+		return this._southWall;
+	}
+	setSouthWallState(newState) {
+		this._southWall = newState;
+	}
+	// getters and setters for West wall state
+	getWestWallState() {
+		return this._westWall;
+	}
+	setWestWallState(newState) {
+		this._westWall = newState;
+	}
+	// getters and setters for Active wall state
+	isActive() {
+		return this._activeSpot;
+	}
+	setIsActive(newState) {
+		this._activeSpot = newState;
+		if (newState === true) { // an active tile has necessarily been visited
+			this._visited = true;
+		}
+	}
+	// getters and setters for Visited wall state
+	isVisited() {
+		return this._visited;
+	}
+	setIsVisited(newState) {
+		this._visited = newState;
 	}
 }
 
@@ -29,10 +80,12 @@ class Cell {
 */
 class Column {
 	constructor(width) {
+		// clamp provided values within the established boundaries - always sanitize input!
+		this._width = Math.clamp(width, MIN_WIDTH, MAX_WIDTH);
 		this._cells = [];
 	
 		// generate a list of blank Cells to fit the required width
-		for (let currCellNum = 0; currCellNum < width; currCellNum++) {
+		for (let currCellNum = 0; currCellNum < this._width; currCellNum++) {
 			this._cells.push(new Cell());
 		}
 	}
@@ -45,10 +98,12 @@ class Column {
 */
 class Row {
 	constructor(width, height) {
+		// clamp provided values within the established boundaries - always sanitize input!
+		this._height = Math.clamp(width, MIN_HEIGHT, MAX_HEIGHT)
 		this._columns = [];
 		
 		// generate a list of columns to fit the required height
-		for (let currColNum = 0; currColNum < height; currColNum++) {
+		for (let currColNum = 0; currColNum < this._height; currColNum++) {
 			this._columns.push(new Column(width));
 		}
 	}
@@ -68,6 +123,3 @@ class MazeMap {
 		this._rows = new Row(width, height);
 	}
 }
-
-let test_map = new MazeMap(7, 3, "NO SOLVER YET IMPLEMENTED");
-console.log(test_map);
